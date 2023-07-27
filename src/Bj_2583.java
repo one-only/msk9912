ed06d502ba9645f43b27c7870a888f8d12fcc611
Ms Kim <myungseokim9912@gmail.com>
2023-07-27 20:30:58 +0900

    rename 2583

--- src/Bj_2583.java
+++ src/Bj_2583.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bj_2583 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int M = sc.nextInt();
