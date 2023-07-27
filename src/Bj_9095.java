b71f0543548ce4fa659ada742904a09ce0ac2545
Ms Kim <myungseokim9912@gmail.com>
2023-07-27 20:22:33 +0900

    first commit

---
+++ src/Bj_9095.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+//3단 피보나치
+public class Bj_9095 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int[] arr = new int[11];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 4;
+        for (int i = 3; i < 11; i++) {
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        }
+        int[] num = new int[count];
+        for (int i = 0; i < count; i++) {
+            num[i] = sc.nextInt();
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(arr[num[i]-1]);
+        }
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int count = sc.nextInt();
+        int[][] arr = new int[M][N];
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        arr[y1][x1] = 1;
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        arr[y2][x2] = 1;
+
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
