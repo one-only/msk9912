fd2a7ad3c1e1328d2ffae28debbe1266374e1690
Ms Kim <myungseokim9912@gmail.com>
2023-08-07 10:26:32 +0900

    10828 스택 추가 풀이

--- src/day0810/Bj_10828_2.java
+++ src/day0810/Bj_10828_2.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Stack;
 
 // 10828번 스택
-public class bj_10828_2 {
+public class Bj_10828_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
