d797efbf22972b29895334380f5e2a8cb59e3906
Ms Kim <myungseokim9912@gmail.com>
2023-09-20 21:40:54 +0900

    백준2960

--- src/day0921/Bj_2960.java
+++ src/day0921/Bj_2960.java
@@ -3,8 +3,6 @@ package day0921;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
