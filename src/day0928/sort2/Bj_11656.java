e797f6c03705d73d4998a58f6175d946a2b2ab84
Ms Kim <myungseokim9912@gmail.com>
2023-09-24 23:52:06 +0900

    백준11656

---
+++ src/day0928/sort2/Bj_11656.java
@@ -0,0 +1,24 @@
+package day0928.sort2;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Bj_11656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String st = br.readLine();
+
+        String[] strArray = new String[st.length()];
+        for (int i = 0; i < st.length(); i++) {
+            strArray[i] = st.substring(i, st.length());
+        }
+        Arrays.sort(strArray);
+
+        for (String str: strArray) {
+            bw.write(str);
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
